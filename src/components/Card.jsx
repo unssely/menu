1408bf948e6b3b8466d8cc8da0d328936ba18c9b
unssely <susarinaulia91@gmail.com>
2026-06-03 10:55:15 +0500

    kkk

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,11 +12,20 @@ return (
             </span>
 
             <span className='price'>
-                {card.price}
+                <h3>{card.price}</h3>
             </span>
         </div>
         <p>{card.occupation}</p>
-        <div className=''></div>
+        <div className='bot'>
+            <span className='plus'>
+                <h3>{card.plus}</h3>
+            </span>
+
+            <span className='star'>
+                {card.star}
+            </span>
+
+        </div>
      </div>
     </div>
 )
